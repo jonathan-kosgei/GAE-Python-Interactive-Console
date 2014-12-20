801e98844f8fd41a17027b700c9d0cdc5e86be2e
Jonathan Trevor <jonathankosgei1000@gmail.com>
2014-12-20 08:32:53 +0000

    :(

--- console-py.py
+++ console-py.py
@@ -1,42 +1,12 @@
 from google.appengine.api import xmpp
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from code import InteractiveConsole
-import sys
-
-class FileCacher:
-    """Cache stdout text so that we can analyze it"""
-    def __init__(self): self.reset()
-    def reset(self): self.out = []
-    def write(self, line):self.out.append(line)
-    def flush(self):
-        output = '\n'.join(self.out)
-        self.reset()
-        return output
-        
-class Shell(InteractiveConsole):
-    """Wrapper around Python that can filter input/output to the shell"""
-    def __init__(self):
-        self.stdout = sys.stdout
-        self.cache = FileCacher()
-        InteractiveConsole.__init__(self)
-        return
-    def get_output(self):sys.stdout = self.cache
-    def return_output(self):sys.stdout = self.stdout
-    def push(self, line):
-        self.get_output()
-        #line = filter(line)
-        InteractiveConsole.push(self, line)
-        self.return_output()
-        output = self.cache.flush()
-        #output = filter(output)
-        print output # or something else
-        
+       
 
 class XMPPHandler(webapp.RequestHandler):
     def post(self):
     	message = xmpp.Message(self.request.POST)
-    	message.reply(sh.interact())
+    	message.reply('hello')
 
  
 
@@ -53,6 +23,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-    sh = Shell()
-    sh.interact()
+ #   sh = Shell()
+ #  sh.interact()
 	
