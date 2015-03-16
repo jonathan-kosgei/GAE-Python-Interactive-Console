5af09c17ee9f2ce155c6fd44867e7b2e97dd50d1
jonathan-kosgei <jonathankosgei1000@gmail.com>
2015-03-16 07:15:15 +0000

    First Commit

--- console-py.py
+++ console-py.py
@@ -1,51 +1,43 @@
+"""Creates a custom Python Interpreter
+Note: I can get this to work on google app engine by commenting out sh.interact()
+and uncommenting sh.push and code = message.body"""
 from google.appengine.api import xmpp
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from code import InteractiveConsole
 import sys
-import StringIO
 
 class FileCacher:
-    """Cache stdout text so that we can analyze it"""
-    def __init__(self): self.reset()
-    def reset(self): self.out = []
-    def write(self, line):self.out.append(line)
+    """Cache stdout text so we can analyze it before returning it."""
+    def __init__(self):self.out = []
+    def reset(self):self.out = []
+    def write (self,line):self.out.append(line)
     def flush(self):
-        output = '\n'.join(self.out)
+        output = ''.join(self.out)
         self.reset()
         return output
-        
-class Shell(InteractiveConsole):
-    """Wrapper around Python that can filter input/output to the shell"""
+
+class Jonathan(InteractiveConsole):
+    """Wrapper around Python that can filter output to the shell."""
     def __init__(self):
-        self.stdin = sys.stdin
         self.stdout = sys.stdout
         self.cache = FileCacher()
-        sys.stdin = StringIO()
-        self.output = ''
         InteractiveConsole.__init__(self)
         return
     def get_output(self):sys.stdout = self.cache
     def return_output(self):sys.stdout = self.stdout
-    def set_input(self, code):sys.stdin.write(code)
-    def reset_input(self):sys.stdin = self.stdin
     def push(self, line):
         self.get_output()
         InteractiveConsole.push(self, line)
         self.return_output()
-        output = self.cache.flush()
-        self.output = output
-        #output = filter(output)
-        print output # or something else
-    def output(self): return self.output()
-      
+        self.output = self.cache.flush()
+        print(self.output)      
 
 class XMPPHandler(webapp.RequestHandler):
     def post(self):
 			message = xmpp.Message(self.request.POST)
-			sh.set_input(message.body)
-			message.reply(sh.output())
-			sh.reset_input()
+            sh.push(message.body)
+            message.reply(sh.output)
  
 
 class MainPage(webapp.RequestHandler):
@@ -60,8 +52,8 @@ def main():
     run_wsgi_app(app)
 
 if __name__ == "__main__":
-    main()
-    sh = Shell()
-    sh.interact()
+    sh = Jonathan()
+    #sh.interact('Enter code to start Sir Jonathan.')
+    #sh.push(code)
    
     
