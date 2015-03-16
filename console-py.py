71d430f206e776b9d8cff35be7f3ba047e89a8d9
jonathan-kosgei <jonathankosgei1000@gmail.com>
2015-03-16 08:04:28 +0000

    First Commit

--- console-py.py
+++ console-py.py
@@ -21,12 +21,17 @@ class Jonathan(InteractiveConsole):
     """Wrapper around Python that can filter output to the shell."""
     def __init__(self):
         self.stdout = sys.stdout
+        self.stderr = sys.stderr
         self.cache = FileCacher()
         InteractiveConsole.__init__(self)
         self.output = ''
         return
-    def get_output(self):sys.stdout = self.cache
-    def return_output(self):sys.stdout = self.stdout
+    def get_output(self):
+        sys.stdout = self.cache
+        sys.stderr - self.cache
+    def return_output(self):
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
     def push(self, line):
         self.get_output()
         InteractiveConsole.push(self, line)
